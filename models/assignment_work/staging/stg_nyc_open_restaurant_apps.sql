e1e2d115a10e3f7524117113ee341949549046cd
Qian Chen <qc1849230373@gmail.com>
2026-03-30 03:34:54 +0000

    clean and transform staging data for nyc_open_restaurant_apps

---
+++ models/assignment_work/staging/stg_nyc_open_restaurant_apps.sql
@@ -0,0 +1,84 @@
+-- Clean and standardize nyc open restaurant applicaqtion data
+-- One row per application
+
+WITH source AS (
+   SELECT * FROM {{ source('raw', 'source_nyc_open_restaurant_apps') }}
+), -- Easier to refer to the dbt reference to a long name table this way
+
+cleaned AS (
+   SELECT
+       -- Get all columns from source, except ones we're transforming below
+       -- To do cleaning on them or explicitly cast them as types just in case
+       * EXCEPT (
+           objectid,
+           globalid,
+           time_of_submission,
+           restaurant_name,
+           legal_business_name,
+           doing_business_as_dba,
+           zip,
+           borough,
+           business_address,
+           street,
+           latitude,
+           longitude,
+       ),
+
+       -- Identifiers
+       CAST(objectid AS STRING) AS application_id,
+       CAST(globalid AS STRING) AS global_id,
+
+       -- Date/Time
+       CAST(time_of_submission AS TIMESTAMP) AS time_of_submission,
+
+       -- Application details
+       CAST(restaurant_name AS STRING) AS restaurant_name,
+       CAST(legal_business_name AS STRING) AS legal_business_name,
+       CAST(doing_business_as_dba AS STRING) AS dba_name,
+       
+       -- Location - clean zip code, handling several common zip code data problems
+       CASE
+           WHEN UPPER(TRIM(CAST(zip AS STRING))) IN ('N/A', 'NA') THEN NULL
+           WHEN UPPER(TRIM(CAST(zip AS STRING))) = 'ANONYMOUS' THEN 'Anonymous'
+           WHEN LENGTH(CAST(zip AS STRING)) = 5 THEN CAST(zip AS STRING)
+           WHEN LENGTH(CAST(zip AS STRING)) = 9 THEN CAST(zip AS STRING)
+           WHEN LENGTH(CAST(zip AS STRING)) = 10
+               AND REGEXP_CONTAINS(CAST(ip AS STRING), r'^\d{5}-\d{4}')
+           THEN CAST(zip AS STRING)
+           ELSE NULL
+       END AS zip,
+
+       -- Location - standardized borough, just in case
+       CASE
+           WHEN UPPER(TRIM(borough)) IN ('MANHATTAN', 'NEW YORK COUNTY') THEN 'Manhattan'
+           WHEN UPPER(TRIM(borough)) IN ('BRONX', 'THE BRONX') THEN 'Bronx'
+           WHEN UPPER(TRIM(borough)) IN ('BROOKLYN', 'KINGS COUNTY') THEN 'Brooklyn'
+           WHEN UPPER(TRIM(borough)) IN ('QUEENS', 'QUEEN', 'QUEENS COUNTY') THEN 'Queens'
+           WHEN UPPER(TRIM(borough)) IN ('STATEN ISLAND', 'RICHMOND COUNTY') THEN 'Staten Island'
+           ELSE 'UNKNOWN or CITYWIDE'
+       END AS borough,
+
+       CAST(business_address AS STRING) AS business_address,
+       CAST(street AS STRING) AS street,
+       CAST(latitude AS DECIMAL) AS latitude,
+       CAST(longitude AS DECIMAL) AS longitude,
+
+       -- Clearer column name as well for this one
+
+       -- Metadata
+       CURRENT_TIMESTAMP() AS _stg_loaded_at
+
+   FROM source
+
+   -- Filters
+   AND objectid IS NOT NULL
+   AND globalid IS NOT NULL
+   AND CAST(time_of_submission AS DATE) >= DATE_SUB(CURRENT_DATE(), INTERVAL 7 YEAR)
+   AND borough IS NOT NULL
+
+   -- Deduplicate
+   QUALIFY ROW_NUMBER() OVER (PARTITION BY objectid ORDER BY time_of_submission DESC) = 1
+)
+
+SELECT * FROM cleaned
+
